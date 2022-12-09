1853889a8250029c31bf3a4e99fe37dcbf2125b0
AvicennaJr <justfuad@protonmail.com>
2022-12-09 16:37:15 +0300

    Started the evaluation
    -> Wrote a simple Evaluator to evaluate integers
    -> Wrote tests for the above

---
+++ evaluator/evaluator.go
@@ -0,0 +1,31 @@
+package evaluator
+
+import (
+	"github.com/AvicennaJr/Nuru/ast"
+	"github.com/AvicennaJr/Nuru/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statment := range stmts {
+		result = Eval(statment)
+	}
+
+	return result
+}
