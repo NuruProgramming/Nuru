e32f32a0f3c5f1a6bb581cae6bcd8101ebddef6b
AvicennaJr <justfuad@protonmail.com>
2023-01-05 23:47:16 +0300

    Add ability to evaluate '.'

--- src/evaluator/dot.go
+++ src/evaluator/dot.go
@@ -5,9 +5,9 @@ import (
 )
 
 func evalDotExpression(left, right object.Object, line int) object.Object {
-	if right.Type() != object.BUILTIN_OBJ {
-		return newError("Mstari %d: '%s' sio function sahihi", line, right.Inspect())
-	}
+	// if right.Type() != object.BUILTIN_OBJ {
+	// 	return newError("Mstari %d: '%s' sio function sahihi", line, right.Inspect())
+	// }
 	switch left.(type) {
 	case *object.String:
 		return evalDotStringExpression(left, right, line)
@@ -21,13 +21,24 @@ func evalDotExpression(left, right object.Object, line int) object.Object {
 }
 
 func evalDotStringExpression(left, right object.Object, line int) object.Object {
+	leftVal := left.(*object.String)
+	rightVal := right.(*object.String)
+
+	switch rightVal.Inspect() {
+	case "idadi":
+		return &object.Integer{Value: int64(len(leftVal.Value))}
+	}
 	return nil
 }
 
 func evalDotDictExpression(left, right object.Object, line int) object.Object {
+	// to do
+
 	return nil
 }
 
 func evalDotArrayExpression(left, right object.Object) object.Object {
+	// to do
+
 	return nil
 }
