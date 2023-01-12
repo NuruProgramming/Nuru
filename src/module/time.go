47b602258db0e494177bd1905bd3214c489df58a
Hopertz <lugano.ngulwa@gmail.com>
2023-01-12 16:45:23 +0300

    ongeza tangu method

--- src/module/time.go
+++ src/module/time.go
@@ -12,6 +12,7 @@ var TimeFunctions = map[string]object.ModuleFunction{}
 func init() {
 	TimeFunctions["hasahivi"] = now
 	TimeFunctions["lala"] = sleep
+	TimeFunctions["tangu"] = since
 }
 
 func now(args []object.Object) object.Object {
@@ -40,3 +41,29 @@ func sleep(args []object.Object) object.Object {
 
 	return nil
 }
+
+func since(args []object.Object) object.Object {
+
+	if len(args) != 1 {
+		return &object.Error{Message : "tunahitaji hoja moja tu kwenye "}
+	}
+
+    t, err := time.Parse("2006-01-02 15:04:05", args[0].Inspect())
+
+	if err != nil {
+		
+		return &object.Error{Message : "tumeshindwa kuparse hoja zako"}
+	}
+
+
+	current_time  := time.Now().Format("2006-01-02 15:04:05")
+	ct, _ := time.Parse("2006-01-02 15:04:05", current_time )
+
+
+
+	diff := ct.Sub(t)
+	durationInSeconds := diff.Seconds()
+
+
+	return &object.Float{Value: durationInSeconds}
+ }
