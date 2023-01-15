262ebd43deacc60c20664915082872630a1f2cc6
AvicennaJr <justfuad@protonmail.com>
2023-01-15 22:58:40 +0300

    Add json and net to modules list

--- src/module/module.go
+++ src/module/module.go
@@ -7,4 +7,6 @@ var Mapper = map[string]*object.Module{}
 func init() {
 	Mapper["os"] = &object.Module{Name: "os", Functions: OsFunctions}
 	Mapper["muda"] = &object.Module{Name: "time", Functions: TimeFunctions}
+	Mapper["mtandao"] = &object.Module{Name: "net", Functions: NetFunctions}
+	Mapper["jsoni"] = &object.Module{Name: "json", Functions: JsonFunctions}
 }
