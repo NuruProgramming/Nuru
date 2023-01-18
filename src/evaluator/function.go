ca9ce08abe827e1ecf77955ed0506088bbba4deb
AvicennaJr <justfuad@protonmail.com>
2023-01-18 23:46:18 +0300

    Add separate eval function

---
+++ src/evaluator/function.go
@@ -0,0 +1,17 @@
+package evaluator
+
+import (
+	"github.com/AvicennaJr/Nuru/ast"
+	"github.com/AvicennaJr/Nuru/object"
+)
+
+func evalFunction(node *ast.FunctionLiteral, env *object.Environment) object.Object {
+	function := &object.Function{
+		Parameters: node.Parameters,
+		Defaults:   node.Defaults,
+		Body:       node.Body,
+		Env:        env,
+	}
+
+	return function
+}
