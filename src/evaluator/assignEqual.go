e5215c9c962f6719af859015c7acdc525810ebd8
AvicennaJr <justfuad@protonmail.com>
2023-03-13 23:49:10 +0300

    add a function to evaluate assign-equal operations separately

---
+++ src/evaluator/assignEqual.go
@@ -0,0 +1,149 @@
+package evaluator
+
+import (
+	"strings"
+
+	"github.com/AvicennaJr/Nuru/ast"
+	"github.com/AvicennaJr/Nuru/object"
+)
+
+func evalAssignEqual(node *ast.AssignEqual, env *object.Environment) object.Object {
+	left := Eval(node.Left, env)
+	if isError(left) {
+		return left
+	}
+
+	value := Eval(node.Value, env)
+	if isError(value) {
+		return value
+	}
+
+	switch node.Token.Literal {
+	case "+=":
+		switch arg := left.(type) {
+		case *object.Integer:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value + val.Value
+				return env.Set(node.Left.Token.Literal, &object.Integer{Value: v})
+			case *object.Float:
+				v := float64(arg.Value) + val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '+=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.Float:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value + float64(val.Value)
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			case *object.Float:
+				v := arg.Value + val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '+=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.String:
+			switch val := value.(type) {
+			case *object.String:
+				v := arg.Value + val.Value
+				return env.Set(node.Left.Token.Literal, &object.String{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '+=' kwa %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		default:
+			return newError("Mstari %d: Huwezi kutumia '+=' na %v", node.Token.Line, arg.Type())
+		}
+	case "-=":
+		switch arg := left.(type) {
+		case *object.Integer:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value - val.Value
+				return env.Set(node.Left.Token.Literal, &object.Integer{Value: v})
+			case *object.Float:
+				v := float64(arg.Value) - val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '-=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.Float:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value - float64(val.Value)
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			case *object.Float:
+				v := arg.Value - val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '-=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		default:
+			return newError("Mstari %d: Huwezi kutumia '-=' na %v", node.Token.Line, arg.Type())
+		}
+	case "*=":
+		switch arg := left.(type) {
+		case *object.Integer:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value * val.Value
+				return env.Set(node.Left.Token.Literal, &object.Integer{Value: v})
+			case *object.Float:
+				v := float64(arg.Value) * val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '*=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.Float:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value * float64(val.Value)
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			case *object.Float:
+				v := arg.Value * val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '*=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.String:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := strings.Repeat(arg.Value, int(val.Value))
+				return env.Set(node.Left.Token.Literal, &object.String{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '+=' kwa %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		default:
+			return newError("Mstari %d: Huwezi kutumia '*=' na %v", node.Token.Line, arg.Type())
+		}
+	case "/=":
+		switch arg := left.(type) {
+		case *object.Integer:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value / val.Value
+				return env.Set(node.Left.Token.Literal, &object.Integer{Value: v})
+			case *object.Float:
+				v := float64(arg.Value) / val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '/=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		case *object.Float:
+			switch val := value.(type) {
+			case *object.Integer:
+				v := arg.Value / float64(val.Value)
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			case *object.Float:
+				v := arg.Value / val.Value
+				return env.Set(node.Left.Token.Literal, &object.Float{Value: v})
+			default:
+				return newError("Mstari %d: Huwezi kutumia '/=' kujumlisha %v na %v", node.Token.Line, arg.Type(), val.Type())
+			}
+		default:
+			return newError("Mstari %d: Huwezi kutumia '/=' na %v", node.Token.Line, arg.Type())
+		}
+	default:
+		return newError("Mstari %d: Operesheni Haifahamiki  %s", node.Token.Line, node.Token.Literal)
+	}
+}
