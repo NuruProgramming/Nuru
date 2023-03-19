1a8d8af435968e88771b7da7c33c32332b4163e6
AvicennaJr <justfuad@protonmail.com>
2023-03-20 02:54:41 +0300

    Add ability to evaluate default parameters in functions

--- evaluator/function.go
+++ evaluator/function.go
@@ -7,6 +7,7 @@ import (
 
 func evalFunction(node *ast.FunctionLiteral, env *object.Environment) object.Object {
 	function := &object.Function{
+		Name:       node.Name,
 		Parameters: node.Parameters,
 		Defaults:   node.Defaults,
 		Body:       node.Body,
