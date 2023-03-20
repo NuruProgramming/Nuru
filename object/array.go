c599844a8bd565a923b0301b81aa6781b53a0018
AvicennaJr <justfuad@protonmail.com>
2023-03-20 03:49:52 +0300

    Add array methods
    idadi, sukuma and yamwisho

--- object/array.go
+++ object/array.go
@@ -38,3 +38,36 @@ func (ao *Array) Next() (Object, Object) {
 func (ao *Array) Reset() {
 	ao.offset = 0
 }
+
+func (a *Array) Method(method string, args []Object) Object {
+	switch method {
+	case "idadi":
+		return a.len(args)
+	case "sukuma":
+		return a.push(args)
+	case "yamwisho":
+		return a.last()
+	default:
+		return newError("Samahani, function hii haitumiki na Strings (Neno)")
+	}
+}
+
+func (a *Array) len(args []Object) Object {
+	if len(args) != 0 {
+		return newError("Samahani, tunahitaji Hoja 0, wewe umeweka %d", len(args))
+	}
+	return &Integer{Value: int64(len(a.Elements))}
+}
+
+func (a *Array) last() Object {
+	length := len(a.Elements)
+	if length > 0 {
+		return a.Elements[length-1]
+	}
+	return &Null{}
+}
+
+func (a *Array) push(args []Object) Object {
+	a.Elements = append(a.Elements, args...)
+	return a
+}
