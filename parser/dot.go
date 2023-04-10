788c2ac030ae9780a39494096d427bced8ea279b
AvicennaJr <justfuad@protonmail.com>
2023-04-10 07:34:44 +0300

    Add ability to parse named args

--- parser/dot.go
+++ parser/dot.go
@@ -13,6 +13,28 @@ func (p *Parser) parseMethod(obj ast.Expression) ast.Expression {
 	if !p.expectPeek(token.LPAREN) {
 		return nil
 	}
-	exp.Arguments = p.parseExpressionList(token.RPAREN)
+
+	exp.Defaults = make(map[string]ast.Expression)
+
+	for !p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		if p.curTokenIs(token.COMMA) {
+			continue
+		}
+		if p.peekTokenIs(token.ASSIGN) {
+			name := p.curToken.Literal
+			p.nextToken()
+			p.nextToken()
+			val := p.parseExpression(LOWEST)
+			exp.Defaults[name] = val
+		} else {
+			exp.Arguments = append(exp.Arguments, p.parseExpression(LOWEST))
+		}
+	}
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
 	return exp
 }
