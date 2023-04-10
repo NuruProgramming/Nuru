2ea4573f9a65ad04461565d50ba1224a905106fb
AvicennaJr <justfuad@protonmail.com>
2023-04-10 07:35:34 +0300

    add ability to add specific durations (seconds, minutes etc)

--- object/time.go
+++ object/time.go
@@ -11,21 +11,55 @@ type Time struct {
 
 func (t *Time) Type() ObjectType { return TIME_OBJ }
 func (t *Time) Inspect() string  { return t.TimeValue }
-func (t *Time) Method(method string, args []Object) Object {
+func (t *Time) Method(method string, args []Object, defs map[string]Object) Object {
 	switch method {
 	case "ongeza":
-		return t.add(args)
+		return t.add(args, defs)
 
 	}
 	return nil
 }
 
-func (t *Time) add(args []Object) Object {
+func (t *Time) add(args []Object, defs map[string]Object) Object {
+	if len(defs) != 0 {
+		var sec, min, hr, d, m, y int
+		for k, v := range defs {
+			objvalue := v.Inspect()
+			inttime, err := strconv.Atoi(objvalue)
+			if err != nil {
+				return newError("namba tu zinaruhusiwa kwenye hoja")
+			}
+			switch k {
+			case "sekunde":
+				sec = inttime
+			case "dakika":
+				min = inttime
+			case "saa":
+				hr = inttime
+			case "siku":
+				d = inttime
+			case "miezi":
+				m = inttime
+			case "miaka":
+				y = inttime
+			default:
+				return newError("Hukuweka muda sahihi")
+			}
+		}
+		cur_time, _ := time.Parse("15:04:05 02-01-2006", t.Inspect())
+		next_time := cur_time.
+			Add(time.Duration(sec)*time.Second).
+			Add(time.Duration(min)*time.Minute).
+			Add(time.Duration(hr)*time.Hour).
+			AddDate(y, m, d)
+		return &Time{TimeValue: string(next_time.Format("15:04:05 02-01-2006"))}
+	}
+
 	if len(args) != 1 {
 		return newError("Samahani, tunahitaji Hoja 1, wewe umeweka %d", len(args))
 	}
 
-	cur_time, _ := time.Parse("2006-01-02 15:04:05", t.Inspect())
+	cur_time, _ := time.Parse("15:04:05 02-01-2006", t.Inspect())
 
 	objvalue := args[0].Inspect()
 	inttime, err := strconv.Atoi(objvalue)
@@ -35,5 +69,5 @@ func (t *Time) add(args []Object) Object {
 	}
 
 	next_time := cur_time.Add(time.Duration(inttime) * time.Hour)
-	return &Time{TimeValue: string(next_time.Format("2006-01-02 15:04:05"))}
+	return &Time{TimeValue: string(next_time.Format("15:04:05 02-01-2006"))}
 }
