a97037a0fc3caa40fbaab9fc8e2f90f0d38a108f
AvicennaJr <justfuad@protonmail.com>
2023-04-13 22:31:39 +0300

    add os module with exec and exit methods

--- module/os.go
+++ module/os.go
@@ -2,6 +2,8 @@ package module
 
 import (
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/AvicennaJr/Nuru/object"
 )
@@ -10,14 +12,45 @@ var OsFunctions = map[string]object.ModuleFunction{}
 
 func init() {
 	OsFunctions["toka"] = exit
+	OsFunctions["kimbiza"] = run
 }
 
 func exit(args []object.Object, defs map[string]object.Object) object.Object {
-	if len(args) != 0 {
-		return &object.Null{}
+	if len(args) > 1 {
+		return &object.Error{Message: "Hoja sii sahihi"}
+	}
+
+	if len(args) == 1 {
+		status, ok := args[0].(*object.Integer)
+		if !ok {
+			return &object.Error{Message: "Hoja sii namba"}
+		}
+		os.Exit(int(status.Value))
+		return nil
 	}
 
 	os.Exit(0)
 
 	return nil
+}
+
+func run(args []object.Object, defs map[string]object.Object) object.Object {
+	if len(args) != 1 {
+		return &object.Error{Message: "Idadi ya hoja sii sahihi"}
+	}
+
+	cmd, ok := args[0].(*object.String)
+	if !ok {
+		return &object.Error{Message: "Hoja lazima iwe neno"}
+	}
+	cmdMain := cmd.Value
+	cmdArgs := strings.Split(cmdMain, " ")
+	cmdArgs = cmdArgs[1:]
+
+	out, err := exec.Command(cmdMain, cmdArgs...).Output()
+	if err != nil {
+		return &object.Error{Message: "Tumeshindwa kukimbiza komandi"}
+	}
+
+	return &object.String{Value: string(out)}
 }
