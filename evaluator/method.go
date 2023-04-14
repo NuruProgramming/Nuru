a933e47dbfa34956a105cc8b0231bf73ba587e63
AvicennaJr <justfuad@protonmail.com>
2023-04-14 09:32:46 +0300

    add ability to evaluate package methods

--- evaluator/method.go
+++ evaluator/method.go
@@ -20,10 +20,10 @@ func evalMethodExpression(node *ast.MethodExpression, env *object.Environment) o
 	for k, v := range node.Defaults {
 		defs[k] = Eval(v, env)
 	}
-	return applyMethod(obj, node.Method, args, defs)
+	return applyMethod(obj, node.Method, args, defs, node.Token.Line)
 }
 
-func applyMethod(obj object.Object, method ast.Expression, args []object.Object, defs map[string]object.Object) object.Object {
+func applyMethod(obj object.Object, method ast.Expression, args []object.Object, defs map[string]object.Object, l int) object.Object {
 	switch obj := obj.(type) {
 	case *object.String:
 		return obj.Method(method.(*ast.Identifier).Value, args)
@@ -36,6 +36,20 @@ func applyMethod(obj object.Object, method ast.Expression, args []object.Object,
 	case *object.Module:
 		if fn, ok := obj.Functions[method.(*ast.Identifier).Value]; ok {
 			return fn(args, defs)
+		}
+	case *object.Instance:
+		if fn, ok := obj.Package.Scope.Get(method.(*ast.Identifier).Value); ok {
+			fn.(*object.Function).Env.Set("@", obj)
+			ret := applyFunction(fn, args, l)
+			fn.(*object.Function).Env.Del("@")
+			return ret
+		}
+	case *object.Package:
+		if fn, ok := obj.Scope.Get(method.(*ast.Identifier).Value); ok {
+			fn.(*object.Function).Env.Set("@", obj)
+			ret := applyFunction(fn, args, l)
+			fn.(*object.Function).Env.Del("@")
+			return ret
 		}
 	}
 	return newError("Samahani, %s haina function '%s()'", obj.Inspect(), method.(*ast.Identifier).Value)
