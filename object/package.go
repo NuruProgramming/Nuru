cb25192e0c3f2005234a9d2071eb1dc45649a9d7
AvicennaJr <justfuad@protonmail.com>
2023-04-14 09:27:37 +0300

    add package object

---
+++ object/package.go
@@ -0,0 +1,18 @@
+package object
+
+import (
+	"fmt"
+
+	"github.com/AvicennaJr/Nuru/ast"
+)
+
+type Package struct {
+	Name  *ast.Identifier
+	Env   *Environment
+	Scope *Environment
+}
+
+func (p *Package) Type() ObjectType { return PACKAGE_OBJ }
+func (p *Package) Inspect() string {
+	return fmt.Sprintf("Pakeji: %s", p.Name.Value)
+}
