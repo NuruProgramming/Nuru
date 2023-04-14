8656d7cc3e19072a9766d6a74302e63054041dee
AvicennaJr <justfuad@protonmail.com>
2023-04-14 09:30:12 +0300

    add ability to evaluate property assignment and expression

---
+++ evaluator/property.go
@@ -0,0 +1,63 @@
+package evaluator
+
+import (
+	"github.com/AvicennaJr/Nuru/ast"
+	"github.com/AvicennaJr/Nuru/object"
+)
+
+func evalPropertyExpression(node *ast.PropertyExpression, env *object.Environment) object.Object {
+	left := Eval(node.Object, env)
+	if isError(left) {
+		return left
+	}
+	switch left.(type) {
+	case *object.Instance:
+		obj := left.(*object.Instance)
+		prop := node.Property.(*ast.Identifier).Value
+		if val, ok := obj.Env.Get(prop); ok {
+			return val
+		}
+	case *object.Package:
+		obj := left.(*object.Package)
+		prop := node.Property.(*ast.Identifier).Value
+		if val, ok := obj.Env.Get(prop); ok {
+			return val
+		}
+		// case *object.Module:
+		// 	mod := left.(*object.Module)
+		// 	prop := node.Property.(*ast.Identifier).Value
+		// 	if val, ok := mod.Properties[prop]; ok {
+		// 		return val()
+		// 	}
+	}
+	return newError("Value %s sii sahihi kwenye %s", node.Property.(*ast.Identifier).Value, left.Inspect())
+}
+
+func evalPropertyAssignment(name *ast.PropertyExpression, val object.Object, env *object.Environment) object.Object {
+	left := Eval(name.Object, env)
+	if isError(left) {
+		return left
+	}
+	switch left.(type) {
+	case *object.Instance:
+		obj := left.(*object.Instance)
+		prop := name.Property.(*ast.Identifier).Value
+		if _, ok := obj.Env.Get(prop); ok {
+			obj.Env.Set(prop, val)
+			return NULL
+		}
+		obj.Env.Set(prop, val)
+		return NULL
+	case *object.Package:
+		obj := left.(*object.Package)
+		prop := name.Property.(*ast.Identifier).Value
+		if _, ok := obj.Env.Get(prop); ok {
+			obj.Env.Set(prop, val)
+			return NULL
+		}
+		obj.Env.Set(prop, val)
+		return NULL
+	default:
+		return newError("Imeshindikana ku assign kwenye package %s", left.Type())
+	}
+}
