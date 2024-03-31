65073dd1fedb3ec1f58a9a69d0f1ba85928322eb
Gekko Wrld <gekkowrld@gmail.com>
2024-03-31 22:02:38 +0300

    fix: Add support for types
    Now types are checked before appending to the 'new string'.
    The '%' is truncated before being printed even if the syntax
    turns out to be wrong.
    
    An array is used to keep the type and subsequently checked against.
    This is ineffective speed and perfomance wise.
    A better solution _may be found_ in the future though.
    
    >> No error handling yet
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- builtins/andika.go
+++ builtins/andika.go
@@ -9,26 +9,30 @@ import (
 func Andika(args ...object.Object) object.Object {
 	if len(args) == 0 {
 		fmt.Println("")
-	} else {
-		var arr []string
-		for _, arg := range args {
-			if arg == nil {
-				return newError("Hauwezi kufanya operesheni hii")
-			}
-			arr = append(arr, arg.Inspect())
-		}
-
-		passedType := Aina(args[0]).Inspect()
-		if passedType == "NENO" {
-			nStr := arr[1:]
-			drrr := formatString(arr[0], nStr)
-			fmt.Println(drrr)
-			return nil
-		}
-
-		str := strings.Join(arr, " ")
-		fmt.Println(str)
+		return nil
 	}
+
+	var arr []string
+	var types []string
+	for _, arg := range args {
+		if arg == nil {
+			return newError("Hauwezi kufanya operesheni hii")
+		}
+		arr = append(arr, arg.Inspect())
+		types = append(types, Aina(arg).Inspect())
+	}
+
+	passedType := types[0]
+	if passedType == "NENO" {
+		nStr := arr[1:]
+		types = types[1:]
+		drrr := formatString(arr[0], nStr, types)
+		fmt.Println(drrr)
+		return nil
+	}
+
+	str := strings.Join(arr, " ")
+	fmt.Println(str)
 	return nil
 }
 
@@ -36,14 +40,22 @@ func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
 }
 
-func formatString(fmt string, vals []string) string {
+func formatString(fmt string, vals []string, types []string) string {
 	var newStr strings.Builder
 	pos := 0
+	lv := len(vals)
 	for i := 0; i < len(fmt); i++ {
 		if fmt[i] == '%' && i+1 < len(fmt) {
 			switch fmt[i+1] {
-			case 's', 'd':
-				if pos < len(vals) {
+			case 's':
+				if pos < lv && (types[pos] == "NENO") {
+					newStr.WriteString(vals[pos])
+					pos++
+					i++
+					continue
+				}
+			case 'd':
+				if pos < lv && (types[pos] == "NAMBA") {
 					newStr.WriteString(vals[pos])
 					pos++
 					i++
