0409cb38f4fe58f2e209a1e5adeccaab0ed6777f
Gekko Wrld <gekkowrld@gmail.com>
2024-04-01 00:27:40 +0300

    chore: Add more format specifiers and docs
    Now the code can handle gracefully:
    
    - boolean
    - arrays
    
    Error handling has been enhanced to acceptable level (whatever that is).
    Type mismatch is now reported appropriately.
    If there is a mismatch in format specifiers to the actual arguments, then an
    error is raised.
    This only happens if the format specifiers are more than the passed arguments.
    This doesn't apply the other way round.
    This is consistent with how C handles this same situation in lax situation
    (not tested in strict and golang though).
    
    Changed the allowed specifiers to match their swahili names, making it easier
    for swahili learners to pick up the specifiers.
    This can be reverted though as it is mostly convention and not hard rules.
    
    Code comments have been included to try and break down the code logic.
    Repetetive part(s) have only been explained once.
    This is to help maintainers and developers skim through the logic with the
    help of the comments and understand it.
    I know that this doensn't substitute clean readable code but it sure does help.
    
    Format specifiers:
    
    - %n -> Nambari
    - %t -> Tungo (ama neno)
    - %b -> Buliani (kweli, sikweli)
    - %s -> Safu (ama orodha)
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- builtins/andika.go
+++ builtins/andika.go
@@ -18,19 +18,27 @@ func Andika(args ...object.Object) object.Object {
 		if arg == nil {
 			return newError("Hauwezi kufanya operesheni hii")
 		}
-		arr = append(arr, arg.Inspect())
-		types = append(types, Aina(arg).Inspect())
+		arr = append(arr, arg.Inspect())           // Append the arguments (strings) into an array
+		types = append(types, Aina(arg).Inspect()) // Append the types of the above args (string format) to an array
 	}
 
 	passedType := types[0]
 	if passedType == "NENO" {
-		nStr := arr[1:]
+		fmts := arr[1:] // Choose from index 1, this is to skip the 'format' string
 		types = types[1:]
-		drrr := formatString(arr[0], nStr, types)
-		fmt.Println(drrr)
+		drrr, err := formatString(arr[0], fmts, types) // call the formatter to do its thing :)
+		// Handle errors, they are returned in string format so that I can call the newError
+		// which takes in a string as the first argument.
+		// This also allows for a central point of handling (or failure in some cases)
+		if err != "" {
+			return newError(err)
+		}
+		fmt.Println(drrr) // print the formatted string
 		return nil
 	}
 
+	// Incase the first argument wasn't a string, then do what was already the default.
+	// This is to preserve some kind of 'backward compatibility'
 	str := strings.Join(arr, " ")
 	fmt.Println(str)
 	return nil
@@ -40,31 +48,106 @@ func newError(format string, a ...interface{}) *object.Error {
 	return &object.Error{Message: fmt.Sprintf(format, a...)}
 }
 
-func formatString(fmt string, vals []string, types []string) string {
+// Format the string according to predefined characters
+// Takes in a string -> This contains the format that the partern will follow.
+//
+//	This follows the already established convention (the format part not what is used)
+//	To accommodate the swahili speakers that this is geered to, the following formats are used:
+//		- %n -> Nambari
+//		- %t -> Tungo (ama neno)
+//		- %b -> Buliani (kweli, sikweli)
+//		- %s -> Safu (ama orodha)
+//	This is what can safely be handled by now.
+//
+// The vals array contains the array with the actual values to be displayed.
+// The types array contains the types that will be matched before being
+//
+//	displayed. If a mismatch happens, then it will be used to return an error
+//
+// Two strings are returned:
+//   - The first string is the actual formatted string
+//   - The second string returns an 'error' string.
+//   - It should have been an actual error but the context usage won't allow it.
+func formatString(fmts string, vals []string, types []string) (string, string) {
 	var newStr strings.Builder
-	pos := 0
+	pos := 0 // The position in the passed arrays
 	lv := len(vals)
-	for i := 0; i < len(fmt); i++ {
-		if fmt[i] == '%' && i+1 < len(fmt) {
-			switch fmt[i+1] {
+
+	// Check if the vals and types array match
+	if lv != len(types) {
+		return "", "Orodha ya majibu hailingani, ripoti hii shida tafadhali"
+	}
+	// Define the error messages
+	srf := "Safu mbadala ni chini ya safu halisi"
+	frf := "Siwezi kupata fomati inayofaa"
+	mrf := "Mismatch ya Fomati"
+
+	// Loop through the string passed.
+	for i := 0; i < len(fmts); i++ {
+		if fmts[i] == '%' && i+1 < len(fmts) {
+			switch fmts[i+1] {
+			case 't':
+				if pos < lv {
+					if types[pos] == "NENO" { // Check if it is what is says
+						newStr.WriteString(vals[pos]) // Pull the value from the array
+						pos++                         // Move the position forwards
+						i++                           // Move the string forwards
+						continue                      // Skip the format specifier
+					} else { // Check if there was a mismatch in types
+						fmrf := fmt.Sprintf("%s: %s si NENO", mrf, types[pos])
+						return "", fmrf
+					}
+				} else {
+					return "", srf // Check if there was a mismatch in length
+				}
+			case 'n':
+				if pos < lv {
+					if types[pos] == "NAMBA" {
+						newStr.WriteString(vals[pos])
+						pos++
+						i++
+						continue
+					} else {
+						fmrf := fmt.Sprintf("%s: %s si NAMBARI", mrf, types[pos])
+						return "", fmrf
+					}
+				} else {
+					return "", srf
+				}
 			case 's':
-				if pos < lv && (types[pos] == "NENO") {
-					newStr.WriteString(vals[pos])
-					pos++
-					i++
-					continue
+				if pos < lv {
+					if types[pos] == "ORODHA" {
+						newStr.WriteString(vals[pos])
+						pos++
+						i++
+						continue
+					} else {
+						fmrf := fmt.Sprintf("%s: %s si ORODHA", mrf, types[pos])
+						return "", fmrf
+					}
+				} else {
+					return "", srf
 				}
-			case 'd':
-				if pos < lv && (types[pos] == "NAMBA") {
-					newStr.WriteString(vals[pos])
-					pos++
-					i++
-					continue
+			case 'b':
+				if pos < lv {
+					if types[pos] == "BOOLEAN" {
+						newStr.WriteString(vals[pos])
+						pos++
+						i++
+						continue
+					} else {
+						fmrf := fmt.Sprintf("%s: %s si BOOLEAN", mrf, types[pos])
+						return "", fmrf
+					}
+				} else {
+					return "", srf
 				}
+			default:
+				return "", frf // Check for mismatch in format specifier
 			}
 		} else {
-			newStr.WriteByte(fmt[i])
+			newStr.WriteByte(fmts[i])
 		}
 	}
-	return newStr.String()
+	return newStr.String(), ""
 }
