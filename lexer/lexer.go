f192a1f82f02c38a42b2682c4101ecb2f80ff422
Gekko Wrld <gekkowrld@gmail.com>
2024-07-05 14:42:25 +0300

    fix: Count column and lines better
    Previuos implementation counted the rows and columns inconsistently.
    This fix will enable counting to be consistent and better.
    
    - Use strings.Builder in `readString` function instead of naive string concatenation
    	This reduces memory allocation and deallocation being done by go making the program efficient.
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,6 +4,7 @@ package lexer
 
 import (
 	"github.com/NuruProgramming/Nuru/token"
+	"strings"
 )
 
 type Lexer struct {
@@ -36,7 +37,6 @@ func (l *Lexer) readChar() {
 }
 
 func (l *Lexer) NextToken() token.Token {
-	l.col += 1
 	var tok token.Token
 	l.skipWhitespace()
 
@@ -60,23 +60,32 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.ASSIGN, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune(';'):
+		l.col++
 		tok = token.Token{Type: token.SEMICOLON, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune('('):
+		l.col++
 		tok = token.Token{Type: token.LPAREN, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune(')'):
+		l.col++
 		tok = token.Token{Type: token.RPAREN, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune('{'):
+		l.col++
 		tok = token.Token{Type: token.LBRACE, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune('}'):
+		l.col++
 		tok = token.Token{Type: token.RBRACE, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune(','):
+		l.col++
 		tok = token.Token{Type: token.COMMA, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune('+'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.PLUS_ASSIGN, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
 		} else if l.peekChar() == rune('+') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.PLUS_PLUS, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -84,11 +93,14 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.PLUS, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('-'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.MINUS_ASSIGN, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
 		} else if l.peekChar() == rune('-') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.MINUS_MINUS, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -96,7 +108,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.MINUS, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('!'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -104,7 +118,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.BANG, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('/'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.SLASH_ASSIGN, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -112,11 +128,14 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.SLASH, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('*'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.ASTERISK_ASSIGN, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
 		} else if l.peekChar() == rune('*') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.POW, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -124,7 +143,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.ASTERISK, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('<'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.LTE, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -132,7 +153,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.LT, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('>'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.GTE, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -140,6 +163,7 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.GT, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case rune('"'), rune('\''):
+		l.col++
 		tok.Type = token.STRING
 		tok.Line.Start.Line = l.line
 		tok.Line.Start.Column = l.col
@@ -158,19 +182,25 @@ func (l *Lexer) NextToken() token.Token {
 	case rune('.'):
 		tok = token.Token{Type: token.DOT, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 	case rune('&'):
+		l.col++
 		if l.peekChar() == rune('&') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.AND, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
 		}
 	case rune('|'):
+		l.col++
 		if l.peekChar() == rune('|') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.OR, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
 		}
 	case rune('%'):
+		l.col++
 		if l.peekChar() == rune('=') {
+			l.col++
 			ch := l.ch
 			l.readChar()
 			tok = token.Token{Type: token.MODULUS_ASSIGN, Literal: string(ch) + string(l.ch), Filename: filename, Line: dts(l.line, l.col)}
@@ -178,28 +208,40 @@ func (l *Lexer) NextToken() token.Token {
 			tok = token.Token{Type: token.MODULUS, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	case 0:
+		l.col++
 		tok.Literal = ""
 		tok.Type = token.EOF
 		tok.Filename = filename
 		tok.Line = sts(l.line, l.col)
 	default:
 		if isLetter(l.ch) {
+			tok.Line.Start.Line = l.line
+			tok.Line.Start.Column = l.col
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			tok.Filename = filename
-			tok.Line = sts(l.line, l.col)
+			tok.Line.End.Line = l.line
+			tok.Line.End.Column = l.col
 			return tok
 		} else if isDigit(l.ch) && isLetter(l.peekChar()) {
+			tok.Line.Start.Line = l.line
+			tok.Line.Start.Column = l.col
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			tok.Filename = filename
-			tok.Line = sts(l.line, l.col)
+			tok.Line.End.Line = l.line
+			tok.Line.End.Column = l.col
 			return tok
 		} else if isDigit(l.ch) {
+			tok.Line.Start.Line = l.line
+			tok.Line.Start.Column = l.col
 			tok = l.readDecimal()
 			tok.Filename = filename
+			tok.Line.End.Line = l.line
+			tok.Line.End.Column = l.col
 			return tok
 		} else {
+			l.col++
 			tok = token.Token{Type: token.ILLEGAL, Literal: string(l.ch), Filename: filename, Line: sts(l.line, l.col)}
 		}
 	}
@@ -232,6 +274,11 @@ func (l *Lexer) readIdentifier() string {
 	position := l.position
 
 	for isLetter(l.ch) || isDigit(l.ch) {
+		l.col++
+		if l.ch == '\n' {
+			l.line++
+			l.col = 1
+		}
 		l.readChar()
 	}
 	return string(l.input[position:l.position])
@@ -245,7 +292,7 @@ func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		if l.ch == '\n' {
 			l.line++
-			l.col = 1
+			l.col = 0
 		}
 		l.readChar()
 	}
@@ -258,6 +305,7 @@ func isDigit(ch rune) bool {
 func (l *Lexer) readNumber() string {
 	position := l.position
 	for isDigit(l.ch) {
+		l.col++
 		l.readChar()
 	}
 	return string(l.input[position:l.position])
@@ -267,6 +315,7 @@ func (l *Lexer) readDecimal() token.Token {
 	integer := l.readNumber()
 	if l.ch == '.' && isDigit(l.peekChar()) {
 		l.readChar()
+		l.col++
 		fraction := l.readNumber()
 		fl := len(fraction)
 		return token.Token{Type: token.FLOAT, Literal: integer + "." + fraction, Line: xts(l.line, l.col-fl, 0, fl)}
@@ -302,12 +351,14 @@ func (l *Lexer) skipMultiLineComment() {
 	endFound := false
 
 	for !endFound {
+		l.col++
 		if l.ch == 0 {
 			endFound = true
 		}
 
 		if l.ch == '*' && l.peekChar() == '/' {
 			endFound = true
+			l.col++
 			l.readChar()
 		}
 
@@ -318,14 +369,17 @@ func (l *Lexer) skipMultiLineComment() {
 }
 
 func (l *Lexer) readString(delim rune) string {
-	var str string
+	var str strings.Builder
 	for {
+		l.col++
 		l.readChar()
 		if l.ch == delim || l.ch == 0 {
 			break
 		} else if l.ch == '\n' {
 			l.line++
+			l.col = 0
 		} else if l.ch == '\\' {
+			l.col++
 			switch l.peekChar() {
 			case 'n':
 				l.readChar()
@@ -342,9 +396,12 @@ func (l *Lexer) readString(delim rune) string {
 			case '\\':
 				l.readChar()
 				l.ch = '\\'
+			case '\'':
+				l.readChar()
+				l.ch = '\''
 			}
 		}
-		str += string(l.ch)
+		str.WriteRune(l.ch)
 	}
-	return str
+	return str.String()
 }
